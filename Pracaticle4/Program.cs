fe09a70e23d06eb403e209d3c24b26dda80dcdad
unknown <meetoza5555@gmail.com>
2020-01-20 13:28:14 +0530

    praticale4 Project

---
+++ Pracaticle4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracaticle4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          
+                string name;
+                string Country;
+                Console.WriteLine("Please Enter Your name");
+                name = Console.ReadLine();
+                Console.WriteLine("Where are you from");
+                Country = Console.ReadLine();
+                Console.WriteLine("my name is " + name + ".I from" + Country);
+                Console.Read();
+
+            
+        }
+    }
+}
